8c84ea2a3083a514ce7e08293539b03a368337c2
Martin Oppitz <6279703+deleonio@users.noreply.github.com>
2024-03-27 07:58:05 +0100

    chore: update dod.yml and npmrc

--- csr/vue-webpack/src/App.vue
+++ csr/vue-webpack/src/App.vue
@@ -1,34 +1,33 @@
 <template>
-	<div className="itzbund container mx-auto my-10 max-w-800px">
-		<header className="text-center">
-			<div className="grid grid-cols-2 items-center">
-				<div>
-					<KolKolibri className="block m-auto w-40" :_labeled="false"></KolKolibri>
+	<div class="itzbund container mx-auto my-10 max-w-800px">
+		<header class="text-center">
+			<div class="grid grid-cols-2 items-center">
+				<div class="block m-auto w-40">
+					<KolKolibri :_labeled="false"></KolKolibri>
 				</div>
 				<div>
-					<img className="block m-auto h-25" :src="logo" alt="Logo vom React Framework" />
+					<img class="block m-auto h-25" src="./assets/logo.vue.png" alt="Logo vom React Framework" />
 				</div>
 			</div>
-			<strong className="text-2xl">Willkommen zu KoliBri</strong>
+			<strong class="text-2xl">Willkommen zu KoliBri</strong>
 		</header>
-		<main className="grid md:grid-cols-2 gap-6 p-12">
-			<KolAlert :_type="'success'" _heading="Dokumentation" _variant="card" :_level="2">
-				<KolLink _href="https://public-ui.github.io/" _label="" _target="_blank">
-					<KolIcon _label="" _icons="fa-sharp fa-solid fa-book" /> Dokumentation öffnen
-				</KolLink>
+		<main class="grid md:grid-cols-2 gap-6 p-12">
+			<KolAlert _type="success" _label="Dokumentation" _variant="card" :_level="2">
+				<KolLink _href="https://public-ui.github.io/" _icons="fa-sharp fa-solid fa-book" _label="Dokumentation öffnen" _target="_blank" />
 			</KolAlert>
-			<KolAlert :_type="'info'" _heading="Theming" _variant="card" :_level="2">
-				<KolLink _href="https://public-ui.github.io/docs/concepts/styling/designer" _label="" _target="_blank">
-					<KolIcon _label="" _icons="fa-solid fa-palette" /> KoliBri-Designer öffnen
-				</KolLink>
+			<KolAlert _type="info" _label="Theming" _variant="card" :_level="2">
+				<KolLink
+					_href="https://public-ui.github.io/docs/concepts/styling/designer"
+					_icons="fa-solid fa-palette"
+					_label="KoliBri-Designer öffnen"
+					_target="_blank"
+				/>
 			</KolAlert>
-			<KolAlert :_type="'warning'" _heading="Mitwirken" _variant="card" :_level="2">
-				<KolLink _href="https://github.com/public-ui/kolibri/" _label="" _target="_blank">
-					<KolIcon _label="" _icons="fa-brands fa-github" /> GitHub öffnen
-				</KolLink>
+			<KolAlert _type="warning" _label="Mitwirken" _variant="card" :_level="2">
+				<KolLink _href="https://github.com/public-ui/kolibri/" _icons="fa-brands fa-github" _label="GitHub öffnen" _target="_blank" />
 			</KolAlert>
-			<KolAlert :_type="'error'" _heading="Kontakt" _variant="card" :_level="2">
-				<KolLink _href="mailto:kolibri@itzbund.de" _label="" _target="_blank"> <KolIcon _label="" _icons="fa-solid fa-envelope" /> E-Mail schreiben </KolLink>
+			<KolAlert _type="error" _label="Kontakt" _variant="card" :_level="2">
+				<KolLink _href="mailto:kolibri@itzbund.de" _icons="fa-solid fa-envelope" _label="E-Mail schreiben" _target="_blank" />
 			</KolAlert>
 		</main>
 	</div>
@@ -36,7 +35,6 @@
 
 <script lang="ts">
 import { KolKolibri, KolIcon, KolLink, KolAlert } from '@public-ui/vue';
-import IMG_FRAMEWORK from './assets/logo.vue.png';
 
 export default {
 	components: {
@@ -48,7 +46,6 @@ export default {
 	setup() {
 		return {
 			label: 'My Button',
-			logo: IMG_FRAMEWORK,
 			on: {
 				onClick: console.log,
 			},
